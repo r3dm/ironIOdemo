9aa5a41afb1e1049d9031253d7327d2c132c9efa
Harry Moreno <morenoh149@gmail.com>
2015-04-06 15:38:02 -0700

    execute dotenv

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+require('dotenv').load()
 var iron_mq = require('iron_mq');
-// var imq = new iron_mq.Client();
-var imq = new iron_mq.Client({token: process.env.IRON_TOKEN,
-                             project_id: process.env.IRON_PROJECT_ID })
+var imq = new iron_mq.Client();
+console.log('token', process.env.IRON_TOKEN)
+console.log('id', process.env.IRON_PROJECT_ID)
+// var imq = new iron_mq.Client({token: process.env.IRON_TOKEN,
+//                              project_id: process.env.IRON_PROJECT_ID })
 
 var queue = imq.queue("test_queue");
 
--- readme.md
+++ readme.md
@@ -2,3 +2,7 @@
 
 ## usage
 place `IRON_TOKEN` and `IRON_PROJECT_ID` in .env file at root of project
+
+run
+
+    % node index
